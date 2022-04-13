9d6e29c74ad3acef40c3e41a4646f3076b72f536
twinnerhenock <demessiehe@gmail.com>
2022-04-13 19:30:25 +0000

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,10 @@
 void print_alphabet(void)
 /**
  * print_alphabet - prints the alphbets in lower case
- * 
  * Description: ends with a new line
  */
 {
 	char letter;
-
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
