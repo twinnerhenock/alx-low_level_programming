65fa4b1b2c3ab882150bf18278c49179ed63eb15
twinnerhenock <demessiehe@gmail.com>
2022-04-13 17:59:22 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  * main - Prints "_putchar" followed by a new line.
  * Return: Always 0.
-*/ 
+ */
 int main(void)
 {
 	 _putchar('_');
@@ -14,5 +15,6 @@ int main(void)
 	 _putchar('a');
 	 _putchar('r');
 	 _putchar('\n');
+
 	return (0);
 }
