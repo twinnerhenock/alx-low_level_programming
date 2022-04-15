6b6dbabf4f8c1661f8590b0f1947da5d851e2e2e
twinnerhenock <demessiehe@gmail.com>
2022-04-15 14:25:21 +0000

    zero is zero

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,13 @@
 
 int main(void)
 {
-        int i;
+        int i=0;
 
-        i = 0;
-        positive_or_negative(i);
-
+        if (i>0)
+		printf("%d is positive\n",i);
+	else if (i<0)
+		printf("%d is negative\n",i);
+	else
+		printf("%d is zero\n",i);
         return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,11 +3,11 @@
 
 #include <stdio.h>
 
-/* MAIN_H */
+
 void positive_or_negative(int i);
 int main(void);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif 
+#endif /* MAIN_H */ 
