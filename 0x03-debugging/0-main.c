2e2e97685e0151ad55720d91e7c2777d7f1300a8
twinnerhenock <demessiehe@gmail.com>
2022-04-15 15:11:18 +0000

    zero is zero

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* main - Tests function that prints if integer is positive or negative.
-* Returns: Always returns 0.
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-        int i=0;
+        int i;
 
-        if (i==0)
-		printf("%d is zero\n",i);
-	else 
-		printf("%d is not zero\n",i);
+        i = 0;
+        positive_or_negative(i);
+
         return (0);
 }
