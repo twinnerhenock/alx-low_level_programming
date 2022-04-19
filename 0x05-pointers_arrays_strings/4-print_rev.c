213aa22ba13276d9675e78dab1daa1af94904ca5
twinnerhenock <demessiehe@gmail.com>
2022-04-19 20:30:36 +0000

    reverse_print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,12 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int index = 0;
 
-	while (s[index++])
-		len++;
+	while (s[index] != '\0')
+		index++;
 
-	for (index = len - 1; index >= 0; index--)
+	for (index = index - 1; index >= 0; index--)
 		_putchar(s[index]);
 
 	_putchar('\n');
