28a88fa73a205a49ec8ad3eef2ce2db8cc465dd7
twinnerhenock <demessiehe@gmail.com>
2022-04-19 19:18:55 +0000

    print_reverse

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -16,7 +16,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
 void _puts(char *str);
-void print_rev(char *);
+void print_rev(char *str);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
