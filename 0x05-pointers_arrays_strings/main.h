3d9db635422dbda2e353c1d38d022a8f5119816e
twinnerhenock <demessiehe@gmail.com>
2022-04-19 17:38:55 +0000

    copy string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@
 
 #include <stdlib.h>
 
+char _strcpy(char *dest, char *src);
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -20,7 +21,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 #endif
