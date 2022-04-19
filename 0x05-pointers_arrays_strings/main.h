bed84c511109c820bd5eac8ea35c22f16f2b3ad6
twinnerhenock <demessiehe@gmail.com>
2022-04-19 17:57:14 +0000

    copy string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@
 
 #include <stdlib.h>
 
-char *_strcpy(char *dest,const char *src);
+char *_strcpy(char *dest,char *src);
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
